80e6707b466ac615dd207f3095a86ae8591e4e0e
rho9306 <363279510@qq.com>
2026-05-13 10:29:26 +0800

    Delete chapt4/chapt4_ws/src/build directory

--- chapt4/chapt4_ws/src/build/chapt4_interfaces/chapt4_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rho9/chapt4/chapt4_ws/src/build/chapt4_interfaces/chapt4_interfaces__py
-# Build directory: /home/rho9/chapt4/chapt4_ws/src/build/chapt4_interfaces/chapt4_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
